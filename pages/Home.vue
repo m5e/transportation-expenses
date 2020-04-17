a528134cbeab56a7f56df8b2e3d4f2c705db191f
treem5e <m-sju-ke13@softbank.ne.jp>
2020-04-18 01:01:01 +0900

    Home.vue layout change

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section>
-      <div class="hero first is-primary is-large">
+      <div class="hero first is-primary is-fullheight">
         <div class="hero-body">
           <div class="hero-container">
             <h1 class="subtitle application-name">
@@ -12,21 +12,30 @@
       </div>
     </section>
     <section>
-      <div class="hero second is-primary is-large">
+      <div class="hero has-background-light is-fullheight">
+        <div class="hero-body">
+          <div class="hero-container introduction">
+            <strong>test</strong>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="hero second is-primary is-fullheight">
         <div class="hero-body">
           <div class="hero-container"></div>
         </div>
       </div>
     </section>
     <section>
-      <div class="hero third is-primary is-large">
+      <div class="hero third is-primary is-fullheight">
         <div class="hero-body">
           <div class="hero-container"></div>
         </div>
       </div>
     </section>
     <section>
-      <div class="hero fourth is-primary is-large">
+      <div class="hero fourth is-primary is-fullheight">
         <div class="hero-body">
           <div class="hero-container"></div>
         </div>
@@ -36,13 +45,43 @@
 </template>
 
 <script>
+// 初期表示時の top 座標
+const DEFALT_SCROLL_TOP = 52;
+
 export default {
   components: {},
   data() {
     return {};
   },
-  mounted() {},
-  methods: {}
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    // コンテンツの表示タイミングを制御
+    handleScroll() {
+      const firstElement = document.getElementsByClassName("first");
+      if (firstElement.length !== 1) return;
+
+      const introductorySentence = document.getElementsByClassName(
+        "introduction"
+      );
+      if (introductorySentence.length !== 1) return;
+
+      const currentScrollY =
+        DEFALT_SCROLL_TOP - firstElement[0].getBoundingClientRect().y;
+
+      // 導入文の表示切り替え
+      if (currentScrollY > 400) {
+        introductorySentence[0].classList.add("introduction-active");
+      } else {
+        introductorySentence[0].classList.remove("introduction-active");
+      }
+    }
+  },
+  // イベントリスナの削除
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 };
 </script>
 
@@ -90,5 +129,27 @@ export default {
   background-attachment: fixed;
   background-repeat: no-repeat;
   background-size: cover;
+}
+.introduction-active {
+  animation: fadeIn 2s ease 0s 1 normal;
+  -webkit-animation: fadeIn 2s ease 0s 1 normal;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
