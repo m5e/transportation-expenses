443dcd597a73c1ff4cca70ce06da57235eb54a02
treem5e <m-sju-ke13@softbank.ne.jp>
2020-04-24 00:16:08 +0900

    Change the button color on the application screen

--- pages/Request.vue
+++ pages/Request.vue
@@ -1,27 +1,17 @@
 <template>
   <section class="request-container">
     <div>
-      <label class="label"> 日付</label>
+      <label class="label">日付</label>
       <input
         type="date"
         class="input"
         :class="{ 'is-danger': requestDate === '' }"
         v-model="requestDate"
       />
-      <span v-if="requestDate === ''" :style="{ color: 'red' }"
-        >日付を設定してください</span
-      >
-      <label class="label">
-        業務内容・行先
-      </label>
-      <textarea
-        class="textarea"
-        :class="{ 'is-danger': content === '' }"
-        v-model="content"
-      />
-      <span v-if="content === ''" :style="{ color: 'red' }"
-        >業務内容もしくは目的地を入力してください</span
-      >
+      <span v-if="requestDate === ''" :style="{ color: 'red' }">日付を設定してください</span>
+      <label class="label">業務内容・行先</label>
+      <textarea class="textarea" :class="{ 'is-danger': content === '' }" v-model="content" />
+      <span v-if="content === ''" :style="{ color: 'red' }">業務内容もしくは目的地を入力してください</span>
       <label class="label">乗り物</label>
       <select class="select" v-model="vehicle">
         <option>電車</option>
@@ -29,70 +19,44 @@
         <option>タクシー</option>
         <option>新幹線</option>
       </select>
-      <label class="label">
-        出発地点
-      </label>
-      <textarea
-        class="textarea"
-        :class="{ 'is-danger': startPoint === '' }"
-        v-model="startPoint"
-      />
-      <span v-if="startPoint === ''" :style="{ color: 'red' }"
-        >出発地点を入力して下さい</span
-      >
+      <label class="label">出発地点</label>
+      <textarea class="textarea" :class="{ 'is-danger': startPoint === '' }" v-model="startPoint" />
+      <span v-if="startPoint === ''" :style="{ color: 'red' }">出発地点を入力して下さい</span>
       <div :style="{ marginTop: '10%' }">
-        <button class="button is-info" @click="refreshhDisplayRelayPoint">
+        <button class="button is-primary" @click="refreshhDisplayRelayPoint">
           {{
-            !isDisplayRelayPoint ? "中継地点を追加する" : "中継地点を削除する"
+          !isDisplayRelayPoint ? "中継地点を追加する" : "中継地点を削除する"
           }}
         </button>
       </div>
       <div v-if="isDisplayRelayPoint">
-        <label class="label">
-          中継地点
-        </label>
+        <label class="label">中継地点</label>
         <textarea
           class="textarea"
           :class="{ 'is-danger': relayPoint === '' }"
           v-model="relayPoint"
         />
-        <span v-if="relayPoint === ''" :style="{ color: 'red' }"
-          >中継地点を入力して下さい</span
-        >
+        <span v-if="relayPoint === ''" :style="{ color: 'red' }">中継地点を入力して下さい</span>
       </div>
-      <label class="label">
-        到着地点
-      </label>
-      <textarea
-        class="textarea"
-        :class="{ 'is-danger': goalPoint === '' }"
-        v-model="goalPoint"
-      />
-      <span v-if="goalPoint === ''" :style="{ color: 'red' }"
-        >到着地点を入力して下さい</span
-      >
+      <label class="label">到着地点</label>
+      <textarea class="textarea" :class="{ 'is-danger': goalPoint === '' }" v-model="goalPoint" />
+      <span v-if="goalPoint === ''" :style="{ color: 'red' }">到着地点を入力して下さい</span>
       <label class="label">請求先</label>
       <select class="select" v-model="request">
         <option>客先</option>
         <option>自社</option>
       </select>
-      <label class="label">
-        金額
-      </label>
+      <label class="label">金額</label>
       <input
         type="number"
         class="input"
         :class="{ 'is-danger': (price <= 0) | (price === '') }"
         v-model="price"
       />
-      <span v-if="price <= 0 || price === ''" :style="{ color: 'red' }"
-        >0 円以上で入力してください</span
-      >
+      <span v-if="price <= 0 || price === ''" :style="{ color: 'red' }">0 円以上で入力してください</span>
       <div class="field is-grouped">
         <p class="control">
-          <a class="button is-info request-button" @click="switchDisplayModal"
-            >申請</a
-          >
+          <a class="button is-primary request-button" @click="switchDisplayModal">申請</a>
         </p>
       </div>
 
@@ -101,11 +65,7 @@
         <div class="modal-card">
           <header class="modal-card-head">
             <p class="modal-card-title">申請確認</p>
-            <button
-              class="delete"
-              aria-label="close"
-              @click="switchDisplayModal"
-            ></button>
+            <button class="delete" aria-label="close" @click="switchDisplayModal"></button>
           </header>
           <section class="modal-card-body">
             下記内容で申請してもよろしいでしょうか？
@@ -121,9 +81,7 @@
           </section>
           <footer class="modal-card-foot">
             <button class="button" @click="switchDisplayModal">戻る</button>
-            <button class="button is-primary" @click="onClickApplyButton">
-              決定
-            </button>
+            <button class="button is-primary" @click="onClickApplyButton">決定</button>
           </footer>
         </div>
       </div>
